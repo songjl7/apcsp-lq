04ce5eb600dd46134b5d06e35bb8e251505fe21b
LiAn Song <songlian7@gmail.com>
2019-10-08 08:48:00 -0600

    Complete bigger_two, fix typos

--- list_quiz.rb
+++ list_quiz.rb
@@ -1,4 +1,4 @@
-# retyrb trye if given lsit has 3 even values all next to each other, anywhere in the list
+# return true if given lsit has 3 even values all next to each other, anywhere in the list
 def three_even?(list)
     countevens = 0
     (list.size - 2).times do |i|
@@ -14,3 +14,18 @@ puts three_even?([2,4,12,5]) # true
 puts three_even?([2,1,4,6]) # false
 puts three_even?([1,4,6,4]) # true
 puts three_even?([]) # false
+
+# given 2 lists of numbers, both length 2, return the list which has the largest sum. in the event of a tie, return the first one given.
+def bigger_two(list1, list2)
+    list1_sum = list1[0] + list1[1]
+    list2_sum = list2[0] + list2[1]
+    if list1_sum < list2_sum
+        print list2,"\n"
+    elsif list2_sum < list1_sum
+        print list1, "\n"
+    else 
+        print list1, "\n"
+    end
+end
+bigger_two([1,2],[3,4])
+bigger_two([1,7],[4,4])
